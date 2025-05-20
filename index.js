7467e57202779233210618564972b42ad9d3c0b4
nserrano123 <nserrano@stone.com.co>
2025-05-20 14:31:18 -0500

    StandaloneQuestion

---
+++ index.js
@@ -0,0 +1,69 @@
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import 'dotenv/config'; // Load environment variables
+
+
+// document.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   progressConversation()
+// })
+
+const openAIApiKey = process.env.OPENAI_API_KEY
+const embeddings = new OpenAIEmbeddings({ openAIApiKey })
+const sbApiKey = process.env.SUPABASE_API_KEY
+const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+const client = createClient(sbUrl, sbApiKey)
+
+const vectorStore = new SupabaseVectorStore(embeddings, {
+  client,
+  tableName: 'documents',
+  queryName: 'match_documents'
+})
+
+const retriever = vectorStore.asRetriever()
+
+const llm = new ChatOpenAI({ openAIApiKey })
+
+// A string holding the phrasing of the prompt
+const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+
+// A prompt created using PromptTemplate and the fromTemplate method
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
+
+// Take the standaloneQuestionPrompt and PIPE the model
+const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm)
+
+// Await the response when you INVOKE the chain. 
+// Remember to pass in a question.
+const response = await standaloneQuestionChain.invoke({
+    question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
+})
+
+const response2 = await retriever.invoke('Will Scrimba work on an old laptop?')
+
+console.log(response2)
+console.log(response)
+
+async function progressConversation() {
+    const userInput = document.getElementById('user-input')
+    const chatbotConversation = document.getElementById('chatbot-conversation-container')
+    const question = userInput.value
+    userInput.value = ''
+
+    // add human message
+    const newHumanSpeechBubble = document.createElement('div')
+    newHumanSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newHumanSpeechBubble)
+    newHumanSpeechBubble.textContent = question
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    // add AI message
+    const newAiSpeechBubble = document.createElement('div')
+    newAiSpeechBubble.classList.add('speech', 'speech-ai')
+    chatbotConversation.appendChild(newAiSpeechBubble)
+    newAiSpeechBubble.textContent = result
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+}
